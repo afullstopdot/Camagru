f43010554ecfb1d868e208772e491b4873cedb6b
afullstopdot <andreantoniomarques19@gmail.com>
2017-01-12 22:06:38 +0200

    Creating landing page for sign in & signup

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,1 +1,14 @@
-Data: <?=print_r($data, true)?>
+<!-- Data: <?=print_r($data, true)?> -->
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <title>Camagru</title>
+    <meta name="viewport" charset="UTF-8" content="width=device-width, initial scale=1">
+    <link rel="stylesheet" href="">
+  </head>
+
+  <body>
+  </body>
+
+</html>
