50ec5febaeb78eca4f9d9856b5ed112edbed2315
Andre MARQUEZ <amarquez@e5r2p5.jnb.42.fr>
2017-05-15 17:50:12 +0200

    Updated installer

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 while true; do
 	read -p "Do you wish to install this program?" yn
 	case $yn in
-		[Yy]* ) mv public ..; mv app ..; mv .htaccess ..; rm -rf Camagru; break;;
+		[Yy]* ) mv public ..; mv app ..; mv .htaccess ..; mv index.php ..; cd ..; rm -rf Camagru; break;;
 		[Nn]* ) exit;;
 		* ) echo "Please answer yes or no.";;
 	esac
